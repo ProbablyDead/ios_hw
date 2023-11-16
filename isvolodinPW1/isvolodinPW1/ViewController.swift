c13e6f47343b62e43384915715a62ad1c98aa0ed
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2023-11-16 19:19:45 +0300

    complete 2nd hw

--- isvolodinPW1/isvolodinPW1/ViewController.swift
+++ isvolodinPW1/isvolodinPW1/ViewController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
+// MARK: - View controller for story board
 class ViewController: UIViewController {
+    // MARK:  All views
     @IBOutlet weak var hatView: UIView!
     @IBOutlet weak var visorView: UIView!
     @IBOutlet var skinView: [UIView]!
@@ -16,6 +18,8 @@ class ViewController: UIViewController {
     @IBOutlet var sweaterView: [UIView]!
     @IBOutlet var pantsView: [UIView]!
     @IBOutlet var shoesView: [UIView]!
+    
+    // MARK: Buttons
     @IBOutlet weak var blinkButton: UIButton!
     @IBOutlet weak var changeOutfitButton: UIButton!
     
@@ -24,10 +28,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    // MARK: Change color of an array of views
     private func changeColor(part: [UIView], color: UIColor? = UIColor.random()) {
         for i in part { i.backgroundColor = color }
     }
     
+    // MARK: Change color for all of views in storyboard
     private func changeOutfitColor() {
         changeColor(part: [ hatView ])
         changeColor(part: [ visorView ], color: hatView.backgroundColor?.darker())
@@ -36,23 +42,28 @@ class ViewController: UIViewController {
         changeColor(part: shoesView)
     }
     
+    // MARK: Change outfit (colors of clouthes and a shape of hat)
     private func changeOutfit() {
         changeOutfitColor()
         hatView.changeShape()
     }
     
-    @objc func unblink(eye: Int) {
-        eyesView[eye].changeShape(radius: UIView.openEyeShape)
-    }
-    
+    // MARK: Change the shapes of a certain eye to closed ones
     private func blink(eye: Int) {
         eyesView[eye].changeShape(radius: UIView.closedEyeShape)
     }
-    
+     
+    // MARK: Change the shapes of a certain eye to open ones
+    @objc func unblink(eye: Int) {
+        eyesView[eye].changeShape(radius: UIView.openEyeShape)
+    }
+   
+    // MARK: Handler of "change outfit" button
     @IBAction func changeOutfitButtonWasPressed(_ sender: Any) {
         changeOutfitButton.isEnabled = false
+        let changeColorAnimationTime = 3.5
         
-        UIView.animate(withDuration: 3.49, animations: {
+        UIView.animate(withDuration: changeColorAnimationTime, animations: {
             self.changeOutfit()
         },
                        completion: { [weak self] _ in
@@ -61,10 +72,12 @@ class ViewController: UIViewController {
         )
     }
     
+    // MARK: Handler of "blink" button
     @IBAction func blinkButtonWasPressed(_ sender: Any) {
         let eye = Int.random(in: 0...1)
+        let blinkAnimationTime = 0.5
         
-        UIView.animate(withDuration: 0.5, animations: {
+        UIView.animate(withDuration: blinkAnimationTime, animations: {
             self.blink(eye: eye)
         },
                        completion: { [weak self] _ in
@@ -76,34 +89,55 @@ class ViewController: UIViewController {
 }
 
 
+// MARK: - Extecsion of the view, that contains default shapes for eyes and the function, changing them
 extension UIView {
     static let closedEyeShape = 9.0
     static let openEyeShape = 0.0
     
+    // MARK: change shape of view
     func changeShape(radius: CGFloat = .random(in: 0...25)) {
         self.layer.cornerRadius = radius
     }
 }
 
+// MARK: - Extension of the color
 extension UIColor {
-    static func random() -> UIColor {
-        UIColor(
-            displayP3Red: .random(in: 0...1),
-            green: .random(in: 0...1),
-            blue: .random(in: 0...1),
-            alpha: 1
-        )
+    // MARK: return color, set using RGB
+    static private func UIColorFromRGB(_ rgbValue: Int) -> UIColor! {
+        return UIColor(
+            red: CGFloat((Float((rgbValue & 0xff0000) >> 16)) / 255.0),
+            green: CGFloat((Float((rgbValue & 0x00ff00) >> 8)) / 255.0),
+            blue: CGFloat((Float((rgbValue & 0x0000ff) >> 0)) / 255.0),
+            alpha: 1.0)
     }
     
-    func lighter(by percentage: CGFloat = 30.0) -> UIColor? {
+    // MARK: return a random hex value
+    static private func randomHex() -> Int {
+        let hex = arc4random_uniform(UInt32(UInt16.max))
+        print(String(format: "%06X", hex))
+        return Int(hex)
+    }
+    
+    // MARK: get random color
+    static func random() -> UIColor {
+        UIColorFromRGB(randomHex())
+    }
+    
+    // MARK: default parameter for adjust method
+    static private let defaultPercentage = 30.0
+    
+    // MARK: make color lighter by percent
+    func lighter(by percentage: CGFloat = defaultPercentage) -> UIColor? {
         return self.adjust(by: abs(percentage) )
     }
     
-    func darker(by percentage: CGFloat = 30.0) -> UIColor? {
+    // MARK: make color darker by percent
+    func darker(by percentage: CGFloat = defaultPercentage) -> UIColor? {
         return self.adjust(by: -1 * abs(percentage) )
     }
     
-    private func adjust(by percentage: CGFloat = 30.0) -> UIColor? {
+    // MARK: change color brightness
+    private func adjust(by percentage: CGFloat = defaultPercentage) -> UIColor? {
         var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
         if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
             return UIColor(red: min(red + percentage/100, 1.0),
---
+++ isvolodinPW2/isvolodinPW2/WishMakerViewController.swift
@@ -0,0 +1,322 @@
+//
+//  WishMakerViewController.swift
+//  isvolodinPW2
+//
+//  Created by Илья Володин on 14.11.2023.
+//
+
+import Foundation
+import UIKit
+
+final class WishMakerViewController: UIViewController {
+    private enum ColorsStrings {
+        static let redStr = "red"
+        static let greenStr = "green"
+        static let blueStr = "blue"
+        static let alphaStr = "alpha"
+        static let formatStringForValues = "%.2f"
+        static let redValueString = "Red: " + formatStringForValues
+        static let greenValueString = "Green: " + formatStringForValues
+        static let blueValueString = "Blue: " + formatStringForValues
+    }
+    
+    private enum Constants {
+        static let appTitle = "HW2 colors"
+        static let description = "Here is my hw 2"
+        static let buttonName = "..."
+        
+        static let min: Double = 0
+        static let max: Int = 1
+        
+        static let foldingTime: TimeInterval = 1
+        static let foldingOffset: CGFloat = 237
+    }
+    
+    private enum Markup {
+        static let titleFontSize: CGFloat = 40
+        static let titleLeadingLabelConstant: CGFloat = 20
+        static let titleTopConstant: CGFloat = 20
+        
+        static let descriptionFontSize: CGFloat = 23
+        static let descriptionLeadingLabelConstant: CGFloat = 20
+        static let descriptionTopConstant: CGFloat = 30
+        
+        static let buttonRightConstant: CGFloat = -10
+        static let buttonBottomConstant: CGFloat = -5
+        
+        static let cornerRadius: CGFloat = 10
+        
+        static let slidersConstant: CGFloat = -10
+        static let bottomSliderConstant: CGFloat = -20
+    }
+    
+    private var titleView = UILabel()
+    private var descriptionView = UILabel()
+    private var sliders: [CustomSlider]?
+    private var button: UIButton?
+    private var folded = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = UIColor.random()
+        
+        configureTitle()
+        configureDescription()
+        configureSliders()
+        configureButton()
+    }
+    
+    private func configureTitle() {
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+        titleView.text = Constants.appTitle
+        titleView.font = UIFont.boldSystemFont(ofSize: Markup.titleFontSize)
+        titleView.textColor = UIColor.random()
+        titleView.layer.masksToBounds = true
+        titleView.layer.cornerRadius = Markup.cornerRadius
+        
+        view.addSubview(titleView)
+        NSLayoutConstraint.activate([
+            titleView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Markup.descriptionLeadingLabelConstant),
+            titleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Markup.descriptionTopConstant)
+        ])
+    }
+    
+    private func configureDescription() {
+        descriptionView.translatesAutoresizingMaskIntoConstraints = false
+        descriptionView.text = Constants.description
+        descriptionView.font = UIFont.systemFont(ofSize: Markup.descriptionFontSize)
+        descriptionView.layer.masksToBounds = true
+        descriptionView.layer.cornerRadius = Markup.cornerRadius
+        descriptionView.backgroundColor = titleView.backgroundColor?.darker()
+        
+        view.addSubview(descriptionView)
+        NSLayoutConstraint.activate([
+            descriptionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            descriptionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Markup.titleLeadingLabelConstant),
+            descriptionView.topAnchor.constraint(equalTo: titleView.bottomAnchor, constant: Markup.titleTopConstant)
+        ])
+    }
+    
+    private func configureSliders() {
+        let colors = Dictionary(uniqueKeysWithValues: zip([ColorsStrings.redStr, ColorsStrings.greenStr, ColorsStrings.blueStr, ColorsStrings.alphaStr], (self.view.backgroundColor?.cgColor.components)!))
+        
+        let redSlider = CustomSlider(title: String(format: ColorsStrings.redValueString, colors[ColorsStrings.redStr]!), min: Constants.min, max: Constants.max, color: colors[ColorsStrings.redStr]!)
+        redSlider.translatesAutoresizingMaskIntoConstraints = false
+        redSlider.backgroundColor = view.backgroundColor
+        
+        let greenSlider = CustomSlider(title: String(format: ColorsStrings.greenValueString, colors[ColorsStrings.greenStr]!), min: Constants.min, max: Constants.max, color: colors[ColorsStrings.greenStr]!)
+        greenSlider.translatesAutoresizingMaskIntoConstraints = false
+        greenSlider.backgroundColor = view.backgroundColor
+        
+        let blueSlider = CustomSlider(title: String(format: ColorsStrings.blueValueString, colors[ColorsStrings.blueStr]!), min: Constants.min, max: Constants.max, color: colors[ColorsStrings.blueStr]!)
+        blueSlider.translatesAutoresizingMaskIntoConstraints = false
+        blueSlider.backgroundColor = view.backgroundColor
+        
+        sliders = [redSlider, greenSlider, blueSlider]
+        
+        view.addSubview(blueSlider)
+        NSLayoutConstraint.activate([
+            blueSlider.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            blueSlider.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            blueSlider.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: Markup.bottomSliderConstant)
+        ])
+        
+        blueSlider.valueChanged = { [weak self] value in
+            let newColor = UIColor(
+                red: colors[ColorsStrings.redStr]!, green: colors[ColorsStrings.greenStr]!, blue: value, alpha: colors[ColorsStrings.alphaStr]!
+            )
+            self?.view.backgroundColor = newColor
+            for slider in self!.sliders! { slider.backgroundColor = newColor }
+            self?.titleView.backgroundColor = newColor.darker()
+            blueSlider.titleView.text = String(format: ColorsStrings.blueValueString, value)
+            self?.descriptionView.backgroundColor = newColor.darker()
+        }
+
+        view.addSubview(greenSlider)
+        NSLayoutConstraint.activate([
+            greenSlider.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            greenSlider.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            greenSlider.bottomAnchor.constraint(equalTo: blueSlider.topAnchor, constant: Markup.slidersConstant)
+        ])
+        
+        greenSlider.valueChanged = { [weak self] value in
+            let newColor = UIColor(
+                red: colors[ColorsStrings.redStr]!, green: value, blue: colors[ColorsStrings.blueStr]!, alpha: colors[ColorsStrings.alphaStr]!
+            )
+            self?.view.backgroundColor = newColor
+            for slider in self!.sliders! { slider.backgroundColor = newColor }
+            greenSlider.titleView.text = String(format: ColorsStrings.greenValueString, value)
+            self?.titleView.backgroundColor = newColor.darker()
+            self?.descriptionView.backgroundColor = newColor.darker()
+        }
+        
+        view.addSubview(redSlider)
+        NSLayoutConstraint.activate([
+            redSlider.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            redSlider.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            redSlider.bottomAnchor.constraint(equalTo: greenSlider.topAnchor, constant: Markup.slidersConstant)
+        ])
+        
+        redSlider.valueChanged = { [weak self] value in
+            let newColor = UIColor(
+                red: value, green: colors[ColorsStrings.greenStr]!, blue: colors[ColorsStrings.blueStr]!, alpha: colors[ColorsStrings.alphaStr]!
+            )
+            self?.view.backgroundColor = newColor
+            for slider in self!.sliders! { slider.backgroundColor = newColor }
+            redSlider.titleView.text = String(format: ColorsStrings.redValueString, value)
+            self?.titleView.backgroundColor = newColor.darker()
+            self?.descriptionView.backgroundColor = newColor.darker()
+        }
+        
+    }
+    
+    private func configureButton() {
+        button = {
+            let control = UIButton()
+            control.backgroundColor = self.view.backgroundColor?.darker()
+            control.setTitle(Constants.buttonName, for: .normal)
+            control.clipsToBounds = false
+            control.translatesAutoresizingMaskIntoConstraints = false
+            control.layer.cornerRadius = Markup.cornerRadius
+            control.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+            control.tag = 1
+            return control
+        }()
+        
+        view.addSubview(button!)
+        button!.rightAnchor.constraint(equalTo: view.rightAnchor, constant: Markup.buttonRightConstant).isActive = true
+        button!.bottomAnchor.constraint(equalTo: self.sliders!.first!.topAnchor, constant: Markup.buttonBottomConstant).isActive = true
+    }
+
+    @objc
+    private func buttonAction(_ sender: UIButton) {
+        let offset = self.folded ? -Constants.foldingOffset : Constants.foldingOffset
+        
+        UIView.animate(withDuration: Constants.foldingTime, animations: {
+            for slider in self.sliders! {
+                slider.frame.origin.y += offset
+            }
+            self.button!.frame.origin.y += offset
+        }, completion: nil)
+        
+        self.folded = !self.folded
+    }
+}
+
+final class CustomSlider: UIView {
+    private enum ConstantStrings {
+        static let error = "init(coder:) has not been implemented"
+    }
+    
+    private enum Markup {
+        static let titleTopConstant: CGFloat = 10
+        static let titlelLeadingConstant: CGFloat = 10
+        
+        static let sliderBottomConstant: CGFloat = -10
+        static let sliderLeadingConstant: CGFloat = 20
+    }
+    
+    var valueChanged: ((Double) -> Void)?
+    var slider = UISlider()
+    var titleView = UILabel()
+    
+    init(title: String, min: Double, max: Int, color: CGFloat) {
+        super.init(frame: .zero)
+        titleView.text = title
+        slider.minimumValue = Float(min)
+        slider.maximumValue = Float(max)
+        slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
+        slider.value = Float(color)
+        configureUI()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError(ConstantStrings.error)
+    }
+    
+    private func configureUI() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        for view in [slider, titleView] {
+            addSubview(view)
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            titleView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleView.topAnchor.constraint(equalTo: topAnchor, constant: Markup.titleTopConstant),
+            titleView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Markup.titlelLeadingConstant),
+            
+            slider.centerXAnchor.constraint(equalTo: centerXAnchor),
+            slider.topAnchor.constraint(equalTo: titleView.bottomAnchor),
+            slider.bottomAnchor.constraint(equalTo: bottomAnchor, constant: Markup.sliderBottomConstant),
+            slider.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Markup.sliderLeadingConstant)
+        ])
+    }
+    
+    @objc
+    private func sliderValueChanged() {
+        valueChanged?(Double(slider.value))
+    }
+    
+}
+
+extension UIColor {
+    private enum Constants {
+        static let defaultPercentage = 30.0
+        static let multiplier = 0xff0000
+        static let divider: Float = 255.0
+        static let alpha: CGFloat = 1
+    }
+    
+    // MARK: return color, set using RGB
+    static private func UIColorFromRGB(_ rgbValue: Int) -> UIColor! {
+        return UIColor(
+            red: CGFloat((Float((rgbValue & Constants.multiplier) >> 16)) / Constants.divider),
+            green: CGFloat((Float((rgbValue & (Constants.multiplier >> 8)) >> 8)) / Constants.divider),
+            blue: CGFloat((Float((rgbValue & (Constants.multiplier >> 16)) >> 0)) / Constants.divider),
+            alpha: Constants.alpha)
+    }
+    
+    // MARK: return a random hex value
+    static private func randomHex() -> Int {
+        Int(arc4random_uniform(UInt32(UInt16.max)))
+    }
+    
+    // MARK: get random color
+    static func random() -> UIColor {
+        UIColorFromRGB(randomHex())
+    }
+            
+   // MARK: default parameter for adjust method
+    
+    // MARK: make color lighter by percent
+    func lighter(by percentage: CGFloat = Constants.defaultPercentage) -> UIColor? {
+        return self.adjust(by: abs(percentage) )
+    }
+    
+    // MARK: make color darker by percent
+    func darker(by percentage: CGFloat = Constants.defaultPercentage) -> UIColor? {
+        return self.adjust(by: -1 * abs(percentage) )
+    }
+    
+    // MARK: change color brightness
+    private func adjust(by percentage: CGFloat = Constants.defaultPercentage) -> UIColor? {
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
+        if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            return UIColor(red: min(red + percentage/100, 1.0),
+                           green: min(green + percentage/100, 1.0),
+                           blue: min(blue + percentage/100, 1.0),
+                           alpha: alpha)
+        } else {
+            return nil
+        }
+    }
+}
+ 
